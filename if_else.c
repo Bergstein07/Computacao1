d9a3182724b237c3444234680e6403490ffa8da9
Bergstein <126175208+Bergstein07@users.noreply.github.com>
2025-04-02 09:14:41 -0300

    Update if_else.c

--- if_else.c
+++ if_else.c
@@ -1,21 +1,18 @@
 #include<stdio.h>
-int main(){
-
+int main()
+{
     float hora, min, seg;
 
     printf("Digite hora, minuto e segundos: ");
     scanf("%f %f %f", &hora, &min, &seg);
 
-if(hora>1 && min>15){
-
-    printf("Quantidade de Segundos %.0f\n", hora*3600 + min*60 + seg);
-
-}
-
-    else{
-
-    printf("Quantidade de Minutos: %.2f\n", min);
-
-}
-    return 0;
+    if(hora>1 && min>15)
+    {
+        printf("Quantidade de Segundos %.0f\n", hora*3600 + min*60 + seg);
     }
+    else
+    {
+        printf("Quantidade de Minutos: %.2f\n", hora*60 + min + (seg/100));
+    }
+    return 0;
+}
