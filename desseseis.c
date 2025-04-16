e2315eb69c9f3f9ba863130893e32307915ac1d1
Bergstein <126175208+Bergstein07@users.noreply.github.com>
2025-04-16 09:35:12 -0300

    Add files via upload

---
+++ desseseis.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    int n, maior;
+    printf("Digite um numero: ");
+    scanf("%d", &n);
+    while (n>0)
+    {
+        printf("Digite um numero: ");
+        scanf("%d", &n);
+
+        if (n > maior)
+        {
+            maior = n;
+        }
+    }
+    printf("O maior numero postivo digitado foi: %d\n", maior);
+    return 0;
+}
