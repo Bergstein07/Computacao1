b8f2def67edfff7a7a31337994c0048796f763d9
Bergstein <126175208+Bergstein07@users.noreply.github.com>
2025-04-16 08:01:22 -0300

    Update resto_2.c

--- resto_2.c
+++ resto_2.c
@@ -2,10 +2,10 @@
 #include<locale.h>
 int main()
 {
-    int n, i = 1;
-    printf("Digite um numero: ");
+    int n, i = 1, r2;
+    printf("Digite o numero que vc deseja dividir: ");
     scanf("%d", &n );
-
+    printf("Os numeros divididos por %d e tem resto 2 sao: \n", n);
     while(i<=100){
     if(i % n == 2){
     printf("%d\n", i);
