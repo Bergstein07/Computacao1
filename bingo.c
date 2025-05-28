faf9b1be21860a2dca1e46f0555c05a5862d6803
Bergstein <126175208+Bergstein07@users.noreply.github.com>
2025-05-28 09:43:31 -0300

    Update bingo.c

--- bingo.c
+++ bingo.c
@@ -4,15 +4,18 @@
 
 #define N 5
 
-void preencheBingo(int matriz[][N], int nl, int nc) {
+void preencheBingo(int matriz[][N], int nl, int nc)
+{
     int numeros[100];
     int total = 100;
 
-    for (int i = 0; i < total; i++) {
+    for (int i = 0; i < total; i++)
+    {
         numeros[i] = i;
     }
 
-    for (int i = total - 1; i > 0; i--) {
+    for (int i = total - 1; i > 0; i--)
+    {
         int j = rand() % (i + 1);
         int temp = numeros[i];
         numeros[i] = numeros[j];
@@ -20,23 +23,29 @@ void preencheBingo(int matriz[][N], int nl, int nc) {
     }
 
     int k = 0;
-    for (int i = 0; i < nl; i++) {
-        for (int j = 0; j < nc; j++) {
+    for (int i = 0; i < nl; i++)
+    {
+        for (int j = 0; j < nc; j++)
+        {
             matriz[i][j] = numeros[k++];
         }
     }
 }
 
-void imprimeMatriz(int matriz[][N], int nl, int nc) {
-    for (int i = 0; i < nl; i++) {
-        for (int j = 0; j < nc; j++) {
+void imprimeMatriz(int matriz[][N], int nl, int nc)
+{
+    for (int i = 0; i < nl; i++)
+    {
+        for (int j = 0; j < nc; j++)
+        {
             printf("%2d\t", matriz[i][j]);
         }
         printf("\n");
     }
 }
 
-int main() {
+int main()
+{
     int cartela[N][N];
 
     srand(time(NULL));
