4b201d7d6b5434ce6f14fee5e9c085903402aa64
Bergstein <126175208+Bergstein07@users.noreply.github.com>
2025-06-11 10:08:18 -0300

    Create compararstring.c

---
+++ compararstring.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char nomes[3][101], nomenovo[101];
+    int existe = 0;
+
+    for (int i = 0; i < 3; i++) {
+        printf("Digite o %dº nome:\n", i + 1);
+        fgets(nomes[i], 101, stdin);
+        nomes[i][strcspn(nomes[i], "\n")] = '\0';
+    }
+
+    while(1)
+    {
+        printf("Digite o nome para saber se já foi colocado:\n");
+        fgets(nomenovo, 101, stdin);
+        nomenovo[strcspn(nomenovo, "\n")] = '\0';
+
+        for (int i = 0; i < 3; i++) {
+            if (strcmp(nomenovo, nomes[i]) == 0) {
+                existe = 1;
+                break;
+            }
+        }
+
+        if (existe) {
+            printf("Programa finalizado");
+            break;
+        } else {
+            printf("Este nome nao esta entre os tres nomes.\n");
+        }
+    }
+
+    return 0;
+}
